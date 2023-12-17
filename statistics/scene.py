f3c6cdd5da5a3050595ca845e355c58b39e1a0bd
Dan Davison <dandavison7@gmail.com>
2023-12-16 21:00:33 -0500

    Random numbers emerging at random angles

--- statistics/scene.py
+++ statistics/scene.py
@@ -1,3 +1,6 @@
+import random
+from math import cos, sin
+
 import manim as m
 from manim import *
 
@@ -6,14 +9,19 @@ m = m
 
 class RandomThingGenerator(Scene):
     def construct(self):
+        things = [Text(str(i), font_size=72) for i in range(1, 10 + 1)]
+
         circle = Circle()
+        origin = circle.get_center()
+        self.add(circle)
 
-        number_7 = Text("7", font_size=72)
-        number_7.move_to(circle.get_center())
-
-        target_position = number_7.get_center() + RIGHT * 3
-
-        anim = ApplyMethod(number_7.move_to, target_position)
-
-        self.add(circle, number_7)
-        self.play(anim)
+        for _ in range(10):
+            [t] = random.sample(things, 1)
+            theta = 2 * PI * random.random()
+            direction = np.array((cos(theta), sin(theta), 0))
+            target_position = origin + direction * 10
+            self.add(t)
+            t.move_to(origin)
+            anim = ApplyMethod(t.move_to, target_position)
+            self.play(anim)
+            self.wait()
